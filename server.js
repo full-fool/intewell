a9d14dcb0d4508c63bde67ae6ed801ef921c14ca
full-fool <cyq_2210@163.com>
2018-10-01 15:24:27 +0800

    update directory structure

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
+app.use(express.static('public'));
+
 app.get('/*',function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
