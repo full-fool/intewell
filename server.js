5881f4ae718821706f509b42bb09e8a8aef4b6a6
full-fool <cyq_2210@163.com>
2019-01-08 16:02:51 +0800

    add a new module

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+var favicon = require("serve-favicon")
 
 app.use(express.static('public'));
+app.use(favicon(__dirname + "/public/img/favicon.ico"));
 
 app.get('/*',function(req,res){
   res.sendFile(__dirname + '/index.html');
