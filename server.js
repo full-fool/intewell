33638a71da14d3fd5447964af5ba7e92ce0560f0
full-fool <cyq_2210@163.com>
2019-01-09 16:53:35 +0800

    update path

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.get('/*',function(req,res){
 
 // app.listen(80);
 var options = {
-	ca: fs.readFileSync('/certificate/intewelltech_com.ca-bundle'),
-	key: fs.readFileSync('/certificate/intewelltech_com.key'),
-	cert: fs.readFileSync('/certificate/intewelltech_com.crt')
+	ca: fs.readFileSync(__dirname + '/certificate/intewelltech_com.ca-bundle'),
+	key: fs.readFileSync(__dirname + '/certificate/intewelltech_com.key'),
+	cert: fs.readFileSync(__dirname + '/certificate/intewelltech_com.crt')
 }
 http.createServer(app).listen(80);
 https.createServer(options, app).listen(443);
