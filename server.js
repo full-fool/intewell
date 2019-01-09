3dac6b49d4318e504745b4c21e15651d4e5372be
full-fool <cyq_2210@163.com>
2019-01-09 17:21:27 +0800

    add https auto redirect

--- server.js
+++ server.js
@@ -9,15 +9,27 @@ var fs 		= require('fs');;
 app.use(express.static('public'));
 app.use(favicon(__dirname + "/public/img/favicon.ico"));
 
-app.get('/*',function(req,res){
-  res.sendFile(__dirname + '/index.html');
+// app.get('/*',function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+app.get('/*', function (req, res) {
+    if(req.protocol === 'https') {
+        res.sendFile(__dirname + '/index.html');
+    }
+    else {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
 });
 
-// app.listen(80);
 var options = {
 	ca: fs.readFileSync(__dirname + '/certificate/intewelltech_com.ca-bundle'),
 	key: fs.readFileSync(__dirname + '/certificate/intewelltech_com.key'),
 	cert: fs.readFileSync(__dirname + '/certificate/intewelltech_com.crt')
 }
+
 http.createServer(app).listen(80);
 https.createServer(options, app).listen(443);
+
+
+
